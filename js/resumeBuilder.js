4fea71a320f89d20472d3ab190aa54cf9109b0ef
Zoltan Albert <znalbert@gmail.com>
2015-11-28 19:02:34 -0600

    Added bio, edu, work, and projects to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,90 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "Zoltan Albert",
+	"role" : "Front-End Web Developer",
+	"contact" : {
+		"mobile" : "202-455-8679",
+		"email" : "znalbert@gmail.com",
+		"github" : "Demiror",
+		"location" : "Nashville"
+	},
+	"welcomeMessage" : "Hi!",
+	"skills" : [
+		"one", "two", "three"
+	],
+	"bioPic" : "images/fry.jpg"
+};
+
+var education = {
+	"schools" : [
+		{
+			"name" : "Defense Language Institute",
+			"location" : "Monterey, CA",
+			"degree" : "AA",
+			"majors" : ["Mandarin Chinese"],
+			"dates" : "Jun '04 - Oct '05",
+			"url" : "http://www.dliflc.edu/"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Learn to Program: The Fundamentals",
+			"school" : "Coursera:  University of Toronto",
+			"dates" : "Dec 2012",
+			"url" : "https://www.coursera.org/course/programming1"
+		},
+		{
+			"title" : "Intro to HTML and CSS",
+			"school" : "Udacity",
+			"dates" : "Nov 2014",
+			"url" : "https://www.udacity.com/course/ud304"
+		},
+		{
+			"title" : "How to Use Git and GitHub",
+			"school" : "Udacity",
+			"dates" : "Dec 2014",
+			"url" : "https://www.udacity.com/course/ud775"
+		},
+		{
+			"title" : "JavaScript Basics",
+			"school" : "Udacity",
+			"dates" : "Dec 2014",
+			"url" : "https://www.udacity.com/course/ud775"
+		}
+	]
+};
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "US Navy",
+			"title" : "Cryptologic Technician Interpretive",
+			"dates" : "Feb '04 - Feb '10",
+			"location" : "Kunia, HI",
+			"description" : "Chinese linguist at a Secret Compartmented Information Facility."
+		},
+		{
+			"employer" : "Amazon.com",
+			"title" : "Search & Rescue Associate",
+			"dates" : "Nov 12 - Present",
+			"location" : "Huntington, WV",
+			"description" : "Escalated customer service."
+		}
+	]
+};
+
+var projects = {
+	"project" : [
+		{
+			"title" : "Mockup to Website",
+			"dates" : "Dec 2014",
+			"description" : "Replicate in HTML and CSS a design mockup provided in a PDF format.",
+			"images" : "none"
+		},
+		{
+			"title" : "Incorrect Shipping Address Kaizen",
+			"dates" : "Feb 2014",
+			"description" : "Co-led a week long process improvement event to determine how to create a better customer service response to customer's contacting Amazon Customer Service with an incorrect shipping address.",
+			"images" : "none"
+		}
+	]
+};
